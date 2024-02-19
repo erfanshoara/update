ef50b641f337c54c527df8644f022e3a4818eca4
Erfan Shoara <erfan@shoara.net>
2024-02-19 15:41:27 -0500

    change in /bash/*
    updated the var and sourcing

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,54 +1,20 @@
-#####<<<<<-----   common   ----->>>>>#####
-#	rm
-alias rmr='rm -r'
-#	tree
+# ls
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias al='ls -la'
+alias la='ls -a'
+
+# tree
 alias tre='tree -C'
 alias t='tre'
 alias ta='tre -a'
 alias tp='tre -a --prune'
 alias td='tre -d'
 alias tad='tree -Cad'
-#	cp
-alias cp='cp -i'
-#	which type
-alias wch='alias | which -ai'
-alias type='type -a'
-#	cd
-alias cdc='cd ..'
-#	bash + aliases
-alias vimr='vim -R'
-alias vima="vim $BASH_ALIASES"
-alias vimb="vim $BASHRC"
-# TODO
-#alias vimc="vim $XDG_CONFIG_HOME/vim/rc.vim"
-alias b="source $BASH_ALIASES"
-alias a='alias'
-#	basics
-alias q='exit'
-alias c='clear'
-alias off='poweroff'
-alias mv='mv -i'
-alias ln='ln -i'
-alias mk='mkdir'
-alias mkd='function _mkd(){ mkdir $1 $2 && cd $1 ;}; _mkd'
-alias uxme='chmod u+x'
-alias diff='diff --color'
-#	acl
-alias g='getfacl'
-# ls
-alias ls='ls --color=auto'
-alias ll='ls -l'
-alias al='ls -la'
-alias la='ls -a'
-# systemd
-alias s='sudo systemctl'
-alias j='sudo journalctl -xe'
-alias sa='systemctl status'
-# ping
-alias fing='ping -c 1'
 
-#####<<<<<-----   genral   ----->>>>>#####
+# vim
 alias v='vim'
+alias vimr='vim -R'
 alias svim='sudo vim'
 alias vterm='vim -c "term ++curwin"'
 
@@ -60,8 +26,35 @@ alias gitb='git branch -v'
 alias gitr='git remote -v'
 alias gitg='git log --oneline --graph --all --decorate'
 
+# which type
+alias wch='alias | which -ai'
+alias type='type -a'
 
-# appended
+# opening/loading conf file
+alias vimb="vim ${BASH_RUNC}"
+alias vima="vim ${BASH_ALIA}"
+alias vimc="vim ${VIM_RUNC}"
+alias b="source $BASH_ALIA"
+
+# basic shortcuts
+alias q='exit'
+alias c='clear'
+alias a='alias'
+alias off='poweroff'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rmr='rm -r'
+alias cdc='cd ..'
+alias ln='ln -i'
+alias mk='mkdir'
+alias mkd='function _mkd(){ mkdir $1 $2 && cd $1 ;}; _mkd'
+alias uxme='chmod u+x'
+alias diff='diff --color'
+alias g='getfacl'
+alias s='sudo systemctl'
+alias j='sudo journalctl -xe'
+alias sa='systemctl status'
+alias fing='ping -c 1'
 alias py='python3'
 alias l='less'
 alias difr='diff -r'
--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -24,10 +24,6 @@ export ESCDELAY=0
 set -o vi
 PS1='\e[0;31m[\e[0m\e[0;36m\u\e[0m\e[0;34m@\e[0m\e[0;32m\h\e[0m\e[0;35m:\e[0m\e[0;33m\w\e[0m\e[0;31m]\e[0m\n>> '
 
-# if user already has it
-[[ -f "$BASH_ALIASES" ]] && . $BASH_ALIASES
-# }
-
 # general conf
 # {
 umask 0077
